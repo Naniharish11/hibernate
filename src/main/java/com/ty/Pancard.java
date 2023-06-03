1477e12312ed6d025cba42db7caaeb341e875203
Naniharish7 <metikalaharish720@gmail.com>
2023-06-03 12:12:29 +0530

    my hibernate

--- src/main/java/com/ty/Pancard.java
+++ src/main/java/com/ty/Pancard.java
@@ -1,5 +1,45 @@
 package com.ty;
 
-public class Pancard {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Pancard {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+  int id;
+	String name;
+	String add;
+	long phn;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public long getPhn() {
+		return phn;
+	}
+	public void setPhn(long phn) {
+		this.phn = phn;
+	}
+	@Override
+	public String toString() {
+		return "Pancard [id=" + id + ", name=" + name + ", add=" + add + ", phn=" + phn + "]";
+	}
+	
 }
--- src/main/java/com/ty/Person.java
+++ src/main/java/com/ty/Person.java
@@ -1,9 +1,56 @@
 package com.ty;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Person {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY )
+	int id;
+	String name;
+	String add;
+	long phn;
+	@OneToOne Pancard pancard;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public long getPhn() {
+		return phn;
+	}
+	public void setPhn(long phn) {
+		this.phn = phn;
+	}
+	public Pancard getPancard() {
+		return pancard;
+	}
+	public void setPancard(Pancard pancard) {
+		this.pancard = pancard; 
+		
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", add=" + add + ", phn=" + phn + ", pancard=" + pancard + "]";
+	}
+	
 	
 
 }
